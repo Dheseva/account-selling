0d3e571a0f50f6e8a30b92f0ec8a977cdd4d4cbb
catura <catura.ba@gmail.com>
2023-09-20 18:33:12 +0900

    update-user

---
+++ controller/userController.go
@@ -0,0 +1,97 @@
+package controller
+
+import (
+	"account-selling/config"
+	"account-selling/middleware"
+	"account-selling/models"
+	"strconv"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/golang-jwt/jwt/v4"
+)
+
+func UpdateDataUser(c *fiber.Ctx) error {
+	var data map[string]string
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+	id := c.Params("id")
+	idInt, err := strconv.Atoi(id)
+
+	if err != nil {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"status":  false,
+			"message": "invalid request",
+			"data":    nil,
+		})
+	}
+
+	cookie := c.Cookies("jwt")
+	standClaims := &middleware.MyCustomClaims{}
+	convKey := []byte(Secretkey)
+	token, err := jwt.ParseWithClaims(cookie, standClaims, func(t *jwt.Token) (interface{}, error) {
+		return convKey, nil
+	})
+
+	if err != nil {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"status":  false,
+			"error":   err.Error(),
+			"message": "unauthenticated user",
+		})
+	}
+
+	claims := token.Claims.(*middleware.MyCustomClaims)
+
+	var user models.User
+	config.DB.Where("id = ?", claims.Issuer).First(&user)
+
+	var userdata models.UserData
+	config.DB.Where("id = ?", user.UData_id).First(&userdata)
+
+	useridInt := int(user.Id)
+	if idInt != useridInt {
+		c.Status(fiber.StatusBadRequest)
+		return c.JSON(fiber.Map{
+			"status":  false,
+			"message": "unauthenticated user",
+		})
+	}
+	
+	var dateInput int64
+	if data["dateofbirth"] != "" {
+		convertDate := data["dateofbirth"]
+		parsedDate, _ := time.Parse("2006-01-02", convertDate)
+		dateInput = parsedDate.UnixNano() / int64(time.Millisecond)
+	}else{
+		dateInput = 0
+	}
+
+	userdata = models.UserData{
+		Id:          userdata.Id,
+		Nickname:    data["fullname"],
+		Firstname: 	data["firstname"],
+		Lastname: 	data["lastname"],
+		Sex:         data["sex"],
+		Address:     data["address"],
+		Dateofbirth: dateInput,
+		Nationality: data["nationality"],
+		Created_at:  userdata.Created_at,
+		Updated_at:  time.Now().UnixMilli(),
+		Deleted_at:  userdata.Deleted_at,
+	}
+
+	config.DB.Save(&userdata)
+
+	return c.JSON(fiber.Map{
+		"status":  true,
+		"message": "success update data user",
+		"data": fiber.Map{
+			"user":      user,
+			"user_data": userdata,
+		},
+	})
+}
