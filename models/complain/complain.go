a26c36af0e11aeb0dded4256f20940102deb4d92
catura <catura.ba@gmail.com>
2023-09-22 21:33:39 +0900

    models-update

---
+++ models/complain/complain.go
@@ -0,0 +1,11 @@
+package models
+
+type Complain struct {
+	Id         uint   `json:"id"`
+	Items_id   int    `json:"items_id"`
+	User_id    int    `json:"user_id"`
+	Complain   string `json:"complain"`
+	Status     int    `json:"status"` // 1.Review 2.Declined 3.Success
+	Created_at int64  `json:"created_at"`
+	Updated_at int64  `json:"updated_at"`
+}
