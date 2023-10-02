d66475f44bce52af091332845b2b25ccc2cd2694
catura <catura.ba@gmail.com>
2023-10-02 22:50:02 +0900

    router

--- routes/routes.go
+++ routes/routes.go
@@ -14,5 +14,17 @@ func Setup(app *fiber.App) {
 	app.Post("/api/login", controller.Login)
 	app.Get("/api/user", controller.User)
 	app.Get("/api/user/:id", controller.UpdateDataUser)
-	app.Post("/api/logout", controller.Logout)	
+	app.Post("/api/logout", controller.Logout)
+	
+	app.Post("/api/complain", controller.AddComplain)
+	app.Get("/api/complain/:id", controller.UpdateComplain)
+	app.Post("/api/complains/:id", controller.DeleteComplain)
+
+	app.Post("/api/item", controller.AddItems)
+	app.Get("/api/item/:id", controller.UpdateItems)
+	app.Post("/api/items/:id", controller.DeleteItems)
+
+	app.Get("/api/transaction/:id", controller.AddTransaction)
+	app.Get("/api/transactions/:id", controller.UpdateTransaction)
+	
 }
