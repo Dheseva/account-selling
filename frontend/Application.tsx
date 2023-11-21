ad7c175df3891c26dfddddba15380a9397c0c4d9
catura <catura.ba@gmail.com>
2023-11-21 14:37:55 +0900

    update

--- frontend/Application.tsx
+++ frontend/Application.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 // export default function Application() {
 //   return <div>Application</div>;
 // }
--- frontend/pages/Home.tsx
+++ frontend/pages/Home.tsx
@@ -1,10 +1,48 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { styled } from "styled-components";
 
-export default function Home() {
+// assets
+// import { ReactComponent as AboutIcon } from "././public/assets/database.svg";
+
+//Import necessary FontAwesome components
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  backgound-size: cover;
+  background-color: #18a19c;
+`;
+
+const Icon = styled.div`
+  justify-content: right;
+  align-items: right;
+`;
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #333;
+`;
+
+const Homepage: React.FC = () => {
   return (
-    <div>
-      <Link to="/about">About Page</Link>
-    </div>
+    <Container>
+      <Title>Homepage</Title>
+      <br></br>
+      <Icon>
+        <img src="../../public/assets/database.svg" alt="" />
+      </Icon>
+      <br></br>
+      <div>
+        <Link to="/about">
+          <FontAwesomeIcon icon={faInfoCircle} />
+        </Link>
+      </div>
+    </Container>
   );
-}
+};
+
+export default Homepage;
